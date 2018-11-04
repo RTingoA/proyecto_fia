d8097084b29f8357037d881681e8f28d173522d6
Sandro <sandropadilla10@gmail.comm>
2018-11-03 19:20:42 -0500

    commit

---
+++ Migrations/20181103235943_prime.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Proyecto_Fia.Migrations
+{
+    public partial class prime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Mensajes");
+
+            migrationBuilder.CreateTable(
+                name: "Mensas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nombre = table.Column<string>(nullable: false),
+                    Apellido = table.Column<string>(nullable: false),
+                    Dni = table.Column<string>(maxLength: 8, nullable: false),
+                    Correo = table.Column<string>(nullable: false),
+                    Celular = table.Column<string>(maxLength: 9, nullable: false),
+                    Direccion = table.Column<string>(nullable: false),
+                    Texto = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mensas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Mensas");
+
+            migrationBuilder.CreateTable(
+                name: "Mensajes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Correo = table.Column<string>(nullable: false),
+                    Nombre = table.Column<string>(nullable: false),
+                    Texto = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mensajes", x => x.Id);
+                });
+        }
+    }
+}
