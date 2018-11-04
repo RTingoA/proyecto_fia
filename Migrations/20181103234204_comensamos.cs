bc598f612355f886badda76821e5039ac5a08446
RTingoA <rtingoa@gmail.com>
2018-11-03 19:19:15 -0500

    migrations

---
+++ Migrations/20181103234204_comensamos.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Proyecto_Fia.Migrations
+{
+    public partial class comensamos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "correo",
+                table: "Mensajes",
+                newName: "Correo");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Correo",
+                table: "Mensajes",
+                newName: "correo");
+        }
+    }
+}
