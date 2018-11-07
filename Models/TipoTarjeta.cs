232e5441f64332d89818a41b5983738647db2fa6
guillermoormaeche <dylangarcia1996@hotmail.com>
2018-11-06 23:12:03 -0500

    tipo

---
+++ Models/TipoTarjeta.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace Proyecto_Fia.Models{
+
+
+    public class TipoTarjeta{
+
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+    }   
+
+
+}
