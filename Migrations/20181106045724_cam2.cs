3beda9a75ac47b0930156756046a07f37967afd2
jesusEsturol <jesus_estupinan@usmp.pe>
2018-11-06 23:08:46 -0500

    migration

---
+++ Migrations/20181106045724_cam2.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Proyecto_Fia.Migrations
+{
+    public partial class cam2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Sucursales",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Nombre",
+                value: "Sucursal 1");
+
+            migrationBuilder.UpdateData(
+                table: "Sucursales",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Nombre",
+                value: "Sucursal 2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Sucursales",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Nombre",
+                value: "Sucursal Surquillork");
+
+            migrationBuilder.UpdateData(
+                table: "Sucursales",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Nombre",
+                value: "Sucursal San Juan de Luriguashington");
+        }
+    }
+}
