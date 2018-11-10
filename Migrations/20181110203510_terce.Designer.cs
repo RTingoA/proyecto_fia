6ca2c53d39a125f3304fb1f55335306808c031a6
jesusEsturol <jesus_estupinan@usmp.pe>
2018-11-10 17:50:45 -0500

    base

--- Migrations/20181110203510_terce.Designer.cs
+++ Migrations/20181110203510_terce.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Proyecto_Fia.Context;
 
 namespace Proyecto_Fia.Migrations
 {
     [DbContext(typeof(CarWashContext))]
-    partial class CarWashContextModelSnapshot : ModelSnapshot
+    [Migration("20181110203510_terce")]
+    partial class terce
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20181110203510_terce.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Proyecto_Fia.Migrations
+{
+    public partial class terce : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
